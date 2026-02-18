852801ae6759552c116693ea342db7d3eadbe318
contato.geovanigaldino@gmail.com <164828104+geovanigaldino@users.noreply.github.com>
2026-02-18 12:52:23 -0300

    Aula 04

---
+++ src/aula04/Aula04.java
@@ -0,0 +1,13 @@
+package aula04;
+
+public class Aula04 {
+	public static void main(String[] args) {
+		
+		Caneta caneta1 = new Caneta("Nic", 0.4f, "Roxa");
+		caneta1.status();
+		
+		Caneta caneta2 = new Caneta("Faber-castell", 0.7f, "Preta");
+		caneta2.status();
+	}
+
+}
